31275760b2cdfff8e0725c8869f6b34d4a10f862
sepfish <40220408+sepfish@users.noreply.github.com>
2019-04-28 14:28:48 -0700

    Started Level3 Coding Exam
    Almost done with it
    Need to finish method with <String, String>, hashmap1, hashmap2

---
+++ src/HashMapCalculator.java
@@ -0,0 +1,7 @@
+import java.util.HashMap;
+
+public class HashMapCalculator {
+	public HashMap<String, String> commonKeyValuePairs(HashMap<String, String> hashmap) {
+return hashmap;
+	}
+}
--- src/VoteProcessor.java
+++ src/VoteProcessor.java
@@ -1,4 +1,9 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class VoteProcessor {
+	public ArrayList<String> calculateElectionWinner(ArrayList<String> votes) {
+		return votes;
+	}
 
 }
