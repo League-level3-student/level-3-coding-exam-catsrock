9600450671dc2415168ac16781666e8eeca0f3fc
League Student <league@iMac.attlocal.net>
2019-05-12 14:20:54 -0700

    Update HashMapCalculator.java
    Finished level 3 exam yay

--- src/HashMapCalculator.java
+++ src/HashMapCalculator.java
@@ -2,18 +2,19 @@ import java.util.HashMap;
 
 public class HashMapCalculator {
 	public int commonKeyValuePairs(HashMap<String, String> hashmap1, HashMap<String, String> hashmap2) {
-		int count = 0;
-		for (int i = 0; i < hashmap1.size(); i++) {
-			for (int j = 0; j < hashmap2.size(); j++) {
 
-				//if (hashmap1.) {
-				//	count++;
-				//}
-				//if (hashmap1.keySet()==hashmap2.keySet() && hashmap1.values()==hashmap2.values()) {
-					//count++;
-				//}
+		int count = 0;
+		for (String key : hashmap1.keySet()) {
+			if (hashmap1.containsKey(key)) {
+
+				if (hashmap1.get(key).equals(hashmap2.get(key))) {
+					count++;
+				}
+			} else {
+
 			}
 		}
 		return count;
+		
 	}
 }
